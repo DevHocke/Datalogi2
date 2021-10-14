e08acc488494786466373377b72f69c4ef90d937
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-10-14 22:37:07 +0200

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datalogi2
+{
+    class Menu
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datalogi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Menu();
+        }
+    }
+}
